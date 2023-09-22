bf994804e3f693f69bf3348a686f6b1b705daea7
Sofyan Ardianto <sofyanard@gmail.com>
2023-09-22 23:01:44 +0700

    initial commit :seedling:

---
+++ src/main/java/id/go/atrbpn/ocpdemo/OcpdemoApplication.java
@@ -0,0 +1,13 @@
+package id.go.atrbpn.ocpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcpdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcpdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/go/atrbpn/ocpdemo/OcpdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id.go.atrbpn.ocpdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcpdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
